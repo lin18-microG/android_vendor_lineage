9215b78cc913aaa8da722151a304e48415d55918
mse1969 <mse1969@posteo.de>
2025-01-15 00:05:34 +0100

    Custom build release 2025-01-01
    Change-Id: I851c0981f53586fae49f15aeedd12e903a3f4fb3

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2024-12-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2025-01-01
 
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
